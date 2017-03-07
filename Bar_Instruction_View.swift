e8c03692eee881fef574da9241cd71169c96dfbb
Terry Chia <chiawz@hotmail.com>
2017-03-08 01:50:38 +0800

    [Changes]
    Add in animation

--- Bar_Instruction_View.swift
+++ Bar_Instruction_View.swift
@@ -16,9 +16,17 @@ class Bar_Instruction_View: UIViewController {
     @IBOutlet weak var barInstructions: UILabel!
     @IBAction func barCalculate(_ sender: AnyObject) { }
         
-         override func viewDidLoad() {
-            
-            barSeries.image = barSeriesImage
-            
+    override func viewDidLoad() {
+        barSeries.image = barSeriesImage
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        barInstructions.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        UIView.animate(withDuration: 2.0, delay: 0.2, options: .curveEaseOut, animations: { 
+            self.barInstructions.alpha = 1.0
+        }, completion: nil)
     }
 }
